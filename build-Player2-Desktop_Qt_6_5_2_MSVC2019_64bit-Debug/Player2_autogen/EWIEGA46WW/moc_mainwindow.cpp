fb6e92e0c4cdd3af961fcde95ebfa557be4cffe0
Andrey <70484344+Andrey-Moskvey@users.noreply.github.com>
2023-08-08 13:01:13 +0300

    Add files via upload

---
+++ build-Player2-Desktop_Qt_6_5_2_MSVC2019_64bit-Debug/Player2_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -0,0 +1,352 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../Player2/mainwindow.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
+    "MainWindow",
+    "updateTime",
+    "",
+    "updateFrame",
+    "on_pushButtonPlay_clicked",
+    "on_pushButtonSpeed_clicked",
+    "on_pushButtonSize_clicked",
+    "on_SliderTime_sliderPressed",
+    "on_SliderTime_sliderReleased",
+    "on_SliderTime_valueChanged",
+    "value",
+    "changeSpeed",
+    "k",
+    "speedName",
+    "on_radioButtonSpeed_0_toggled",
+    "checked",
+    "on_radioButtonSpeed_1_toggled",
+    "on_radioButtonSpeed_2_toggled",
+    "on_radioButtonSpeed_3_toggled",
+    "on_radioButtonSpeed_4_toggled",
+    "changeSize",
+    "sizeName",
+    "on_radioButtonSize_0_toggled",
+    "on_radioButtonSize_1_toggled",
+    "on_radioButtonSize_2_toggled",
+    "on_radioButtonSize_3_toggled",
+    "on_radioButtonSize_4_toggled"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {
+    uint offsetsAndSizes[54];
+    char stringdata0[11];
+    char stringdata1[11];
+    char stringdata2[1];
+    char stringdata3[12];
+    char stringdata4[26];
+    char stringdata5[27];
+    char stringdata6[26];
+    char stringdata7[28];
+    char stringdata8[29];
+    char stringdata9[27];
+    char stringdata10[6];
+    char stringdata11[12];
+    char stringdata12[2];
+    char stringdata13[10];
+    char stringdata14[30];
+    char stringdata15[8];
+    char stringdata16[30];
+    char stringdata17[30];
+    char stringdata18[30];
+    char stringdata19[30];
+    char stringdata20[11];
+    char stringdata21[9];
+    char stringdata22[29];
+    char stringdata23[29];
+    char stringdata24[29];
+    char stringdata25[29];
+    char stringdata26[29];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSMainWindowENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSMainWindowENDCLASS_t qt_meta_stringdata_CLASSMainWindowENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 10),  // "MainWindow"
+        QT_MOC_LITERAL(11, 10),  // "updateTime"
+        QT_MOC_LITERAL(22, 0),  // ""
+        QT_MOC_LITERAL(23, 11),  // "updateFrame"
+        QT_MOC_LITERAL(35, 25),  // "on_pushButtonPlay_clicked"
+        QT_MOC_LITERAL(61, 26),  // "on_pushButtonSpeed_clicked"
+        QT_MOC_LITERAL(88, 25),  // "on_pushButtonSize_clicked"
+        QT_MOC_LITERAL(114, 27),  // "on_SliderTime_sliderPressed"
+        QT_MOC_LITERAL(142, 28),  // "on_SliderTime_sliderReleased"
+        QT_MOC_LITERAL(171, 26),  // "on_SliderTime_valueChanged"
+        QT_MOC_LITERAL(198, 5),  // "value"
+        QT_MOC_LITERAL(204, 11),  // "changeSpeed"
+        QT_MOC_LITERAL(216, 1),  // "k"
+        QT_MOC_LITERAL(218, 9),  // "speedName"
+        QT_MOC_LITERAL(228, 29),  // "on_radioButtonSpeed_0_toggled"
+        QT_MOC_LITERAL(258, 7),  // "checked"
+        QT_MOC_LITERAL(266, 29),  // "on_radioButtonSpeed_1_toggled"
+        QT_MOC_LITERAL(296, 29),  // "on_radioButtonSpeed_2_toggled"
+        QT_MOC_LITERAL(326, 29),  // "on_radioButtonSpeed_3_toggled"
+        QT_MOC_LITERAL(356, 29),  // "on_radioButtonSpeed_4_toggled"
+        QT_MOC_LITERAL(386, 10),  // "changeSize"
+        QT_MOC_LITERAL(397, 8),  // "sizeName"
+        QT_MOC_LITERAL(406, 28),  // "on_radioButtonSize_0_toggled"
+        QT_MOC_LITERAL(435, 28),  // "on_radioButtonSize_1_toggled"
+        QT_MOC_LITERAL(464, 28),  // "on_radioButtonSize_2_toggled"
+        QT_MOC_LITERAL(493, 28),  // "on_radioButtonSize_3_toggled"
+        QT_MOC_LITERAL(522, 28)   // "on_radioButtonSize_4_toggled"
+    },
+    "MainWindow",
+    "updateTime",
+    "",
+    "updateFrame",
+    "on_pushButtonPlay_clicked",
+    "on_pushButtonSpeed_clicked",
+    "on_pushButtonSize_clicked",
+    "on_SliderTime_sliderPressed",
+    "on_SliderTime_sliderReleased",
+    "on_SliderTime_valueChanged",
+    "value",
+    "changeSpeed",
+    "k",
+    "speedName",
+    "on_radioButtonSpeed_0_toggled",
+    "checked",
+    "on_radioButtonSpeed_1_toggled",
+    "on_radioButtonSpeed_2_toggled",
+    "on_radioButtonSpeed_3_toggled",
+    "on_radioButtonSpeed_4_toggled",
+    "changeSize",
+    "sizeName",
+    "on_radioButtonSize_0_toggled",
+    "on_radioButtonSize_1_toggled",
+    "on_radioButtonSize_2_toggled",
+    "on_radioButtonSize_3_toggled",
+    "on_radioButtonSize_4_toggled"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      20,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,  134,    2, 0x08,    1 /* Private */,
+       3,    0,  135,    2, 0x08,    2 /* Private */,
+       4,    0,  136,    2, 0x08,    3 /* Private */,
+       5,    0,  137,    2, 0x08,    4 /* Private */,
+       6,    0,  138,    2, 0x08,    5 /* Private */,
+       7,    0,  139,    2, 0x08,    6 /* Private */,
+       8,    0,  140,    2, 0x08,    7 /* Private */,
+       9,    1,  141,    2, 0x08,    8 /* Private */,
+      11,    2,  144,    2, 0x08,   10 /* Private */,
+      14,    1,  149,    2, 0x08,   13 /* Private */,
+      16,    1,  152,    2, 0x08,   15 /* Private */,
+      17,    1,  155,    2, 0x08,   17 /* Private */,
+      18,    1,  158,    2, 0x08,   19 /* Private */,
+      19,    1,  161,    2, 0x08,   21 /* Private */,
+      20,    2,  164,    2, 0x08,   23 /* Private */,
+      22,    1,  169,    2, 0x08,   26 /* Private */,
+      23,    1,  172,    2, 0x08,   28 /* Private */,
+      24,    1,  175,    2, 0x08,   30 /* Private */,
+      25,    1,  178,    2, 0x08,   32 /* Private */,
+      26,    1,  181,    2, 0x08,   34 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Float, QMetaType::QString,   12,   13,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Double, QMetaType::QString,   12,   21,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+    QMetaType::Void, QMetaType::Bool,   15,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSMainWindowENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSMainWindowENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
+        // method 'updateTime'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'updateFrame'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButtonPlay_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButtonSpeed_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_pushButtonSize_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_SliderTime_sliderPressed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_SliderTime_sliderReleased'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'on_SliderTime_valueChanged'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<int, std::false_type>,
+        // method 'changeSpeed'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<float, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'on_radioButtonSpeed_0_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSpeed_1_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSpeed_2_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSpeed_3_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSpeed_4_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'changeSize'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<double, std::false_type>,
+        QtPrivate::TypeAndForceComplete<QString, std::false_type>,
+        // method 'on_radioButtonSize_0_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSize_1_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSize_2_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSize_3_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>,
+        // method 'on_radioButtonSize_4_toggled'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        QtPrivate::TypeAndForceComplete<bool, std::false_type>
+    >,
+    nullptr
+} };
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->updateTime(); break;
+        case 1: _t->updateFrame(); break;
+        case 2: _t->on_pushButtonPlay_clicked(); break;
+        case 3: _t->on_pushButtonSpeed_clicked(); break;
+        case 4: _t->on_pushButtonSize_clicked(); break;
+        case 5: _t->on_SliderTime_sliderPressed(); break;
+        case 6: _t->on_SliderTime_sliderReleased(); break;
+        case 7: _t->on_SliderTime_valueChanged((*reinterpret_cast< std::add_pointer_t<int>>(_a[1]))); break;
+        case 8: _t->changeSpeed((*reinterpret_cast< std::add_pointer_t<float>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[2]))); break;
+        case 9: _t->on_radioButtonSpeed_0_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 10: _t->on_radioButtonSpeed_1_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 11: _t->on_radioButtonSpeed_2_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 12: _t->on_radioButtonSpeed_3_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 13: _t->on_radioButtonSpeed_4_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 14: _t->changeSize((*reinterpret_cast< std::add_pointer_t<double>>(_a[1])),(*reinterpret_cast< std::add_pointer_t<QString>>(_a[2]))); break;
+        case 15: _t->on_radioButtonSize_0_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 16: _t->on_radioButtonSize_1_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 17: _t->on_radioButtonSize_2_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 18: _t->on_radioButtonSize_3_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        case 19: _t->on_radioButtonSize_4_toggled((*reinterpret_cast< std::add_pointer_t<bool>>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSMainWindowENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 20)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 20;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 20)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 20;
+    }
+    return _id;
+}
+QT_WARNING_POP
